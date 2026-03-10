7847c7e3c61088a00a9345071dc656cc54e738d3
Petromir Kolev <petromirkolev@gmail.com>
2026-03-10 16:02:40 +0200

    fix: On Track count

--- web/src/utils/serviceItemsHelper.ts
+++ web/src/utils/serviceItemsHelper.ts
@@ -1,5 +1,6 @@
 import type { Maintenance } from '../types/maintenance';
 
 export function checkServiceItemsStatus(item: Maintenance, selectedBike: any) {
-  if (item.bikeId === selectedBike) return item;
+  if (item.date !== null && item.odo !== null && item.bikeId === selectedBike)
+    return item;
 }
